b1298d0546621120ca3e4dfd4f4be3eb0e8c345e
syafiqsafie <syafiqsafie19@gmail.com>
2019-01-07 23:42:41 +0800

    number

--- number.php
+++ number.php
@@ -1,18 +1,48 @@
 <?php
 
-	$i = array(1,2,3,4,5,6,7,8,9,10);
+	$i = array(1,2,35,5,6,7,8,9,10);
 	$v = array(2,4,8,9);
 
-	for($a = 0; $a < 10; $a++)
+	//same array
+	// for($a = 0; $a < 10; $a++)
+	// {
+	// 	for($b = 0; $b < 4; $b++)
+	// 	{
+	// 		if($i[$a] == $v[$b])
+	// 		{
+	// 			echo $i[$a];
+	// 		}
+	// 	}
+	// }
+
+	//average
+	// $w = array_sum($i);
+	// $q = count($i);
+	// $average = $w/$q;
+	// echo $average;
+
+	//highest
+	// $t = 0;
+	// for($a = 0; $a < count($i); $a++)
+	// {
+	// 	if($i[$a] > $t)
+	// 	{
+	// 		$t = $i[$a];
+	// 	}
+	// }
+
+	// echo $t;
+
+	//smallest
+	$t = 100;
+	for($a = 0;$a < count($i); $a++)
 	{
-		for($b = 0; $b < 4; $b++)
+		if($i[$a] < $t)
 		{
-			if($i[$a] == $v[$b])
-			{
-				echo $i[$a];
-			}
+			$t = $i[$a];
 		}
 	}
 
+	echo $t;
 
 ?>
